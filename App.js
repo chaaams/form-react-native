b545a48a7edb8bdab3385493185eb2f70556bdf9
chaaams <diaby.chamssoudine@gmail.com>
2022-04-07 16:18:22 +0200

    form exercices

--- App.js
+++ App.js
@@ -1,21 +1,52 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { StyleSheet, Text, View, SafeAreaView, TextInput, Button } from 'react-native';
 
 export default function App() {
+  const [userName, setUserName] = useState('');
+  const [password, setPassword] = useState('');
+
+  const submit = () =>{
+    const usernameReg = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+    if(usernameReg.test(userName) === true){
+      
+      console.log('userName: ', userName, 'Password: ', password);
+    }else{
+      console.error('email invalide');
+    }
+  }
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+
+    <SafeAreaView style={styles.container}>
+      <Text>Login</Text>
+      <TextInput 
+      placeholder=' Enter Mail'
+      onChangeText={(text) => setUserName(text)}
+      style={styles.input}/>
+
+      <TextInput 
+      placeholder=' Password'
+      onChangeText={(password) => setPassword(password)}
+      secureTextEntry={true}
+      style={styles.input}/>
+
+      <Button title='Connexion' onPress={()=> submit()}/>
       <StatusBar style="auto" />
-    </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'skyblue',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  input: {
+    borderWidth:2,
+    borderColor:'skyblue',
+    margin:15,
+    backgroundColor:'white',
+  }
 });
---
+++ components/Login.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { SafeAreaView, Text } from "react-native";
+
+
+function Login () {
+
+    
+        return (
+            <SafeAreaView>
+                <Text>
+                    login component
+                </Text>
+            </SafeAreaView>)
+    
+}
+
+export default Login
